8ba3bcad49c6ec285a13687ca65bd996cc306738
Jake Jongejans <jake.jongejans@voiceworks.com>
2022-01-11 17:00:22 +0100

    chore(css, fonts): Adds css variables and fonts.

--- index.php
+++ index.php
@@ -5,8 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>ContentDB</title>
+
+    <link href="theme/colours.css" rel="stylesheet">
+    <link href="theme/main.css" rel="stylesheet">
   </head>
   <body>
-    This is not a drill, I repeat, this is not a drill. Get-a-working!
+    <p>This is not a drill, I repeat, this is not a drill. Get-a-working!</p>
   </body>
 </html>
