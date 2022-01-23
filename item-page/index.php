9b839a77b42c67d4c43290df6c34bd77f18cc7e1
Timo <timo.doherty@student.uva.nl>
2022-01-23 21:18:59 +0100

    Added placeholder image

--- item-page/index.php
+++ item-page/index.php
@@ -15,11 +15,7 @@
             const item_element = document.createElement('div');
             item_element.innerHTML = '<item-view title="Spoder" scr="./image/Spiderman.png" public-rating="" private-rating="">test</item-view>';
             document.getElementById('itemlist').appendChild(item_element);
-            // for (let i = 0; i < 10; i++) {
-            //     const list_element = document.createElement('div');
-            //     list_element.innerHTML = '<watch-item title="Spiderman" src="./image/Spiderman.png" rating="3"><p>What is that? A spider?!</p></watch-list>';
-            //     document.getElementById('towatchlist').appendChild(list_element);
-            // }
+
         </script>
     </div>
 </div>
