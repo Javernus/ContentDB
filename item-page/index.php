06d49e6d7ee80f5bfa91b90143bb3287a49061c5
Timo <timo.doherty@student.uva.nl>
2022-01-24 15:36:42 +0100

    Refactoring: removed unneccesary div

--- item-page/index.php
+++ item-page/index.php
@@ -3,21 +3,19 @@
 ?>
 
 
-<div style='justify-content:left; padding-left: 5%; padding-right: 5%;' id="item-page">
+<div style='justify-content:left; padding-left: 5%; padding-right: 5%;' class="item-page" id="item-page">
     <div class='title'>
         <h1>Movie Name</h1>
     </div>
-    <div class='container'>
-        <div id='itemlist' class="item-view"></div>
-        <script>
-            /* Scripts by Timo */
+    <div id='itemlist' class="item-view"></div>
+    <script>
+        /* Scripts by Timo */
 
-            const item_element = document.createElement('div');
-            item_element.innerHTML = '<item-view title="Spoder" src="./image/Spiderman.png" public-rating="" private-rating="3" id="private-rating">test</item-view>';
-            document.getElementById('itemlist').appendChild(item_element);
+        const item_element = document.createElement('div');
+        item_element.innerHTML = '<item-view title="Spoder" src="./image/Spiderman.png" public-rating="" private-rating="3" id="private-rating">test</item-view>';
+        document.getElementById('itemlist').appendChild(item_element);
 
-        </script>
-    </div>
+    </script>
 </div>
 
 <?php
