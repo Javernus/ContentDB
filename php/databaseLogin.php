2b2eeeca517618374df9e6dc461a927509d58976
Jake Jongejans <jake.jongejans@voiceworks.com>
2022-02-03 00:41:03 +0100

    update(db-auth): Environmental variables.

--- php/databaseLogin.php
+++ php/databaseLogin.php
@@ -1,3 +1,8 @@
 <?php
-  $db = new PDO('mysql:host=localhost;dbname=contentdb;charset=utf8', 'Server', 'LKUgVm4eGAiNm9aS');
+  $host = $_ENV["MYSQL_HOST"]
+  $dbname = $_ENV["MYSQL_DBNAME"]
+  $user = $_ENV["MYSQL_USER"]
+  $pass = $_ENV["MYSQL_PASS"]
+
+  $db = new PDO('mysql:host=$host;dbname=$dbname;charset=utf8', $user, $pass);
 ?>
