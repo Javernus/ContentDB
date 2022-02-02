9ad88932440a44dc7d8088c8c576c045ec48198a
montijn van den Beukel <montijn.van.den.beukel@student.uva.nl>
2022-02-02 16:07:33 +0000

    feat(settings page): Adds the settings page with the ability to change the password and delete the account.

---
+++ settings/index.php
@@ -0,0 +1,10 @@
+<?php
+  include '../importables/html-header.php';
+?>
+
+<h1>This is not a drill, I repeat, this is not a drill. Get-a-working!</h1>
+<cdb-dialog open=true><cdb-settings></cdb-settings></cdb-dialog>
+
+<?php
+  include '../importables/html-footer.php';
+?>
